93b609cb1ecd72cf4926e15c1c1dc725e46179ce
Wuwenchie <161718523+Wuwenchie@users.noreply.github.com>
2024-12-26 16:37:18 +0800

    Create recorder.py

---
+++ recorder.py
@@ -0,0 +1,25 @@
+import struct
+from pvrecorder import PvRecorder
+import keyboard
+import wave
+
+recorder = PvRecorder(device_index=-1, frame_length=512)
+audio = []
+
+try:
+    recorder.start()
+    print("Recording... Press Ctrl+C to stop.")
+
+    while True:
+        frame = recorder.read()
+        audio.extend(frame)
+
+except KeyboardInterrupt:
+    recorder.stop()
+    with wave.open('output.wav', 'w') as f:
+        f.setparams((1, 2, 16000, 512, "NONE", "NONE"))
+        f.writeframes(struct.pack("h" * len(audio), *audio))
+    print("Audio saved to output.wav")
+
+finally:
+    recorder.delete()
